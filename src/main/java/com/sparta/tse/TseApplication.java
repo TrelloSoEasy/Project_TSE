5cf369b0f0d50756296ba6e7e4962c99be2fa7b6
multiverse22 <momizi527@gmail.com>
2024-10-14 20:53:37 +0900

    init commit

---
+++ src/main/java/com/sparta/tse/TseApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.tse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/tse/TseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.tse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
