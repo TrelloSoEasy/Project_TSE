4197ef4be9722a588af41fffdf9a3062023ee10e
BoSung308 <qhtjd2719@naver.com>
2024-10-15 12:18:16 +0900

    알림 메세지 commit test(무시해도됨)

--- src/main/java/com/sparta/tse/domain/notification/controller/NotificationController.java
+++ src/main/java/com/sparta/tse/domain/notification/controller/NotificationController.java
@@ -1,2 +1,22 @@
-package com.sparta.tse.domain.notification.controller;public class NotificationController {
+package com.sparta.tse.domain.notification.controller;
+
+
+import com.sparta.tse.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PostMapping("/trello/notify/discord")
+    public ApiResponse<Void> notifyDiscord(@RequestBody String message) {
+        notificationService.sendDiscordNotification(message);
+        return ApiResponse.onSuccess();
+    }
+
 }
--- src/main/java/com/sparta/tse/domain/notification/service/NotificationService.java
+++ src/main/java/com/sparta/tse/domain/notification/service/NotificationService.java
@@ -1,2 +1,23 @@
-package com.sparta.tse.domain.notification.service;public class NotificationService {
+package com.sparta.tse.domain.notification.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final RestTemplate restTemplate;
+
+    private final String discordWebhookUrl =
+            "https://discordapp.com/api/webhooks/1295553870671646791/ETFI6_Nw2-87wzm7iXl1-tG36OBOgXbO5fV1E6EwXXriNMGJ0ky92rajvpRfwCmA4PyC";
+
+    public ApiResponse<Void> sendDiscordNotification(String message){
+        return sendDiscordNotification(discordWebhookUrl, message);
+    }
+
+
+
 }
