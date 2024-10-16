116d8879da6f1b5083fb5bc26b0e6a567ec468e0
lklll1212 <lklll1212@gmail.com>
2024-10-16 14:47:35 +0900

    [fix] 토큰 유효기간 줄이기 (로그아웃) 이 부분 아직 작업전이라 주석처리하고 올립니다.

--- src/main/java/com/sparta/tse/config/JwtUtil.java
+++ src/main/java/com/sparta/tse/config/JwtUtil.java
@@ -66,8 +66,8 @@ public class JwtUtil {
                 .getBody();
     }
 
-    // 토큰 유효기간 강제로 줄이기 (로그아웃)
-    public String logoutWithToken (HttpServletRequest request){
-        this.TOKEN_TIME = 1 * 1000L;
-    }
+//    // 토큰 유효기간 강제로 줄이기 (로그아웃)
+//    public String logoutWithToken (HttpServletRequest request){
+//        this.TOKEN_TIME = 1 * 1000L;
+//    }
 }
