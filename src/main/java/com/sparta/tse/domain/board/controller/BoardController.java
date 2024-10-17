6b5aeadcb2d0dc83ba47186ee8a595bcaddaeb50
jki09871 <ghdwjdrl56@naver.com>
2024-10-18 02:35:42 +0900

    update : Board 파일 업로드 기능 추가

--- src/main/java/com/sparta/tse/domain/board/controller/BoardController.java
+++ src/main/java/com/sparta/tse/domain/board/controller/BoardController.java
@@ -10,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/TSE")
@@ -20,9 +24,11 @@ public class BoardController {
 
     @PostMapping("/{workspaceId}/boards")
     public ApiResponse<BoardPostResponseDto> postBoard(@PathVariable Long workspaceId,
-                                                       @RequestBody BoardPostRequestDto requestDto,
-                                                       @AuthenticationPrincipal AuthUser authUser) {
-        BoardPostResponseDto responseDto = boardService.postBoard(workspaceId,requestDto,authUser);
+                                                       @RequestPart BoardPostRequestDto requestDto,
+                                                       @AuthenticationPrincipal AuthUser authUser,
+                                                       @RequestPart(value = "file", required = false) List<MultipartFile> file) throws IOException {
+        System.out.println("workspaceId = " + workspaceId);
+        BoardPostResponseDto responseDto = boardService.postBoard(workspaceId,requestDto,authUser, file);
         return ApiResponse.onSuccess(responseDto);
     }
 
--- src/main/java/com/sparta/tse/domain/board/dto/response/BoardGetResponseDto.java
+++ src/main/java/com/sparta/tse/domain/board/dto/response/BoardGetResponseDto.java
@@ -1,14 +1,25 @@
 package com.sparta.tse.domain.board.dto.response;
 
+import com.sparta.tse.domain.file.dto.ImagesResponseDto;
+import com.sparta.tse.domain.file.entity.File;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class BoardGetResponseDto {
     private List<ListDto> Lists;
+    private List<ImagesResponseDto> responseDtoList;
+
+    public BoardGetResponseDto(List<ListDto> listDtos, List<File> image) {
+        this.Lists = listDtos;
+        this.responseDtoList = image.stream()
+                .map(ImagesResponseDto::of)
+                .collect(Collectors.toList());
+    }
+
 }
